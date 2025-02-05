a2fec3806bba4593c6fc3d7069bfd15213267724
Ambikesh Srivastava <abhay2004raj15@gmail.com>
2025-02-05 18:03:30 +0530

    first commit

---
+++ question1.py
@@ -0,0 +1,11 @@
+# Create a Numpy array of numbers from 1 to 10 (inclusive).
+
+import numpy as np 
+
+array = np.arange(1, 11)
+print(array)
+print(type(array))
+print(array.shape)
+print(array.size)
+print(array.dtype)
+
